0738bb9e4737e284772c1653e52da21988018f59
kruntoradpetr <petrlunarion@gmail.com>
2021-12-31 22:46:09 +0100

    Variable changes and iprovements

---
+++ src/Controller/DeviceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Exception;
+
+class DeviceController extends AbstractController
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(
+        EntityManagerInterface $em
+    )
+    {
+        $this->em = $em;
+    }
+}
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -116,6 +118,98 @@ class ProfileController extends AbstractController
         return $this->render('admin/profile/update.html.twig', [
             'form' => $form->createView(),
             'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route ("/profile/password-change/{id}", name="profile_password_change")
+     */
+    public function password_change(Request $request, UserPasswordHasherInterface $passwordHasher, User $user)
+    {
+
+        if ($user->getId() != $this->getUser()->getId())
+        {
+            $this->addFlash(
+                'bad',
+                'Nelze měnit hesla jiným uživatelům.'
+            );
+            return $this->redirectToRoute('profile_password_change',['id'=>$this->getUser()->getId()]);
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'invalid_message' => 'Zadaná hesla se musí shodovat.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'error_bubbling'=>true,
+                'first_options'  => [
+                    'label' => 'Heslo',
+                    'error_bubbling'=>true,
+                    'attr' => [
+                        'type' => 'password',
+                        'class'=>'form-control',
+                        'placeholder'=>'Heslo'
+                    ]
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'label' => 'Heslo znovu',
+                        'class'=>'form-control',
+                        'placeholder'=>'Heslo znovu'
+                    ]
+                ],
+            ])
+            ->add('save', SubmitType::class,[
+                'label'=>'Uložit',
+                'attr'=> [
+                    'class'=> 'btn btn-primary',
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+            try {
+
+                $password = $form['password']->getData();
+
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $password
+                );
+
+                $user->setPassword($hashedPassword);
+
+                $this->em->persist($user);
+                $this->em->flush();
+
+                $this->addFlash(
+                    'good',
+                    'Heslo bylo úšpěšně změněno'
+                );
+            }
+            catch (Exception $exception)
+            {
+                $this->addFlash(
+                    'bad',
+                    'Nastala neočekávaná vyjímka.'
+                );
+            }
+        }else{
+            foreach ($form->getErrors(true) as $formError) {
+                $this->addFlash(
+                    'bad',
+                    $formError->getMessage()
+                );
+            }
+        }
+
+        return $this->render('admin/profile/password.html.twig', [
+            'form' => $form->createView(),
+
         ]);
     }
 }
